b64dbe96b9ce71a6d207a50903bce542a4c2e6df
Giacomo Amoroso <giacomo@amoroso.me>
2024-10-20 21:27:15 +0200

    WIP 124 refactoring

--- src/main/java/com/appsdeveloperblog/estore/core/model/FetchUserPaymentDetailsQuery.java
+++ src/main/java/com/appsdeveloperblog/estore/core/model/FetchUserPaymentDetailsQuery.java
@@ -1,8 +1,10 @@
 package com.appsdeveloperblog.estore.core.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class FetchUserPaymentDetailsQuery {
 
   private String userId;
